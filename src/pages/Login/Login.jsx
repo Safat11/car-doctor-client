cca807fb4b599aff84c7d9d55205d5cfe59c429e
Safat11 <safat3622@gmail.com>
2025-06-20 22:23:19 +0600

    (Recap)Create a simple Login page with Firebase integration

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,34 @@
+import img from '../../assets/images/login/login.svg'
+
+const Login = () => {
+
+    const handleLogin = event => {
+        event.preventDefault();
+    }
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="w-1/2 mr-12">
+                    <img src={img} alt="" />
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className="card-body">
+                        <h1 className="text-3xl text-center font-bold">Login</h1>
+                        <form onSubmit={handleLogin}>
+                            <fieldset className="fieldset">
+                                <label className="label">Email</label>
+                                <input type="email" className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input type="password" className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <input className="btn btn-primary mt-6" type="submit" value="Login" />
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
