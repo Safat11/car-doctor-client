150c09a9589ec3e0badf119cb15753d690ed5d52
Safat11 <safat3622@gmail.com>
2025-06-20 17:47:50 +0600

    Set Basic Layout For Footer And Header

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
+import Footer from "../pages/Shared/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import NavBar from "../pages/Shared/NavBar/NavBar";
 
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
