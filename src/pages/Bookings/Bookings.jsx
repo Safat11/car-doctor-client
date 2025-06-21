8ed3747dd3ffc0edd8b333393b5dfe510736f8c6
Safat11 <safat3622@gmail.com>
2025-06-21 23:47:33 +0600

    Module Summary And Updated Booking Using Patch

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import status from "daisyui/components/status";
 
 
 const Bookings = () => {
@@ -12,7 +13,7 @@ const Bookings = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setBookings(data))
-    }, []);
+    }, [url]);
 
     const handleDelete = id => {
         const proceed = confirm('Are you sure you want to delete');
@@ -30,6 +31,28 @@ const Bookings = () => {
                     }
                 })
         }
+    }
+
+    const handleBookingConfirm = id => {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'confirm' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    // updateState
+                    const remaining = bookings.filter(booking => booking._id !== id)
+                    const updated = bookings.find(booking => booking._id === id)
+                    updated.status = 'confirm'
+                    const newBookings = [updated, ...remaining];
+                    setBookings(newBookings);
+                }
+            })
     }
 
     return (
@@ -59,6 +82,7 @@ const Bookings = () => {
                                 key={booking._id}
                                 booking={booking}
                                 handleDelete={handleDelete}
+                                handleBookingConfirm={handleBookingConfirm}
                             ></BookingRow>)
                         }
 
