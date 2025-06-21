047edbe21f4a4492385e4a17c156f22d83fbef99
Safat11 <safat3622@gmail.com>
2025-06-21 17:46:28 +0600

    Extract Booking Service Data

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,29 +4,35 @@ import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
 import CheckOut from "../pages/CheckOut/CheckOut";
+import BookService from "../pages/BookService/BookService";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
     children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
-        {
-          path: 'login',
-          element: <Login></Login>
-        },
-        {
-          path: 'signup',
-          element: <SignUp></SignUp>
-        },
-        {
-          path: 'checkout/:id',
-          element: <CheckOut></CheckOut>,
-          loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
-        }
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'signup',
+        element: <SignUp></SignUp>
+      },
+      {
+        path: 'book/:id',
+        element: <BookService></BookService>,
+        loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+      },
+      {
+        path: 'checkout/:id',
+        element: <CheckOut></CheckOut>,
+        loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+      }
     ]
   },
 ]);
--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -10,45 +10,47 @@ const CheckOut = () => {
         <div>
             <h2>Book Service: {title} </h2>
 
-
-            <form >
-                <div className="card-body">
-                    <form>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-
-                        </div>
-                    </form>
-                    <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
-                        <input className="btn btn-primary btn-block" type="submit" value="Confirm Order" />
+            <form>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <br />
+                        <input type="text" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <br />
+                        <input type="text" placeholder="password" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <br />
+                        <input type="text" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <br />
+                        <input type="text" placeholder="password" className="input input-bordered" />
                     </div>
                 </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-block">Login</button>
+                    <input className="btn btn-primary btn-block" type="submit" value="Order Confirm" />
+                </div>
             </form>
+
+            <div className="card-body">
+
+            </div>
+
         </div>
 
 
