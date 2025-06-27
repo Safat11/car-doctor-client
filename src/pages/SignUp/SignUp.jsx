5c24d0493e9a2c3ac33cbb48e1b102ec1acb359f
Safat11 <safat3622@gmail.com>
2025-06-27 12:49:53 +0600

    Create JWT Token For Sign Up And Social Login Users

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import SocialLogin from '../Shared/SocialLogin/SocialLogin';
 
 
 const SignUp = () => {
@@ -47,6 +48,7 @@ const SignUp = () => {
                             </fieldset>
                         </form>
                         <p className='my-4 text-center'>Already Have An Account? <Link className='text-orange-600 font-bold' to='/login'>Login</Link></p>
+                        <SocialLogin></SocialLogin>
                     </div>
                 </div>
             </div>
